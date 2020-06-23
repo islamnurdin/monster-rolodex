0f63da7a71871f0ee73a4405a21752d21529dbda
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-06-23 13:51:41 -0700

    Updates

---
+++ precache-manifest.9a25ee3574c2add2b126ea34cfe075fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df01db23bcc64945ec6edb3ad2df1740",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "61b00cae56166742e70b",
+    "url": "/monsters-rolodex/static/css/main.d1509e05.chunk.css"
+  },
+  {
+    "revision": "48681213804df59957e7",
+    "url": "/monsters-rolodex/static/js/2.33612ea5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.33612ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b00cae56166742e70b",
+    "url": "/monsters-rolodex/static/js/main.77d0eb40.chunk.js"
+  },
+  {
+    "revision": "f6caa2e23f571c27ca4b",
+    "url": "/monsters-rolodex/static/js/runtime-main.a2ebf91d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
